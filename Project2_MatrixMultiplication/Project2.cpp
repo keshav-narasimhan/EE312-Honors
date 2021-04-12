17dfd30962fd9d3ab792f72d20b66be53f2cefe8
Keshav Narasimhan <52009148+keshav-narasimhan@users.noreply.github.com>
2021-04-12 15:35:12 -0500

    Update Project2.cpp

--- Project2_MatrixMultiplication/Project2.cpp
+++ Project2_MatrixMultiplication/Project2.cpp
@@ -1,7 +1,7 @@
 // matrix-multiplication <Project2.cpp>
 // EE 312 Project 2 submission by
 // Keshav Narasimhan
-// kn9558
+// _____
 // Slip days used: 0
 // Spring 2021
 // Include the contents of this file at the top of every file that you submit, replacing the fields within <>.  Do not keep the <>.
